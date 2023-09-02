3075d402574873267edc041af11cc5ef2272e748
rahil_wisdom <mister.r432000@gmail.com>
2023-09-02 18:43:47 +0700

    modify testing

--- app/app.py
+++ app/app.py
@@ -47,4 +47,4 @@ def peminjaman():
     return render_template("peminjaman.html", peminjaman_buku = peminjaman_buku)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host = "0.0.0.0", port = 5000)
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,24 +1,48 @@
-import requests
+# Mengimpor modul yang diperlukan
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
 
-def test_index():
-    url = "http://localhost:5000/"
-    response = requests.get(url)
-    assert response.status_code == 200
-    assert "Selamat Datang di Perpustakaan" in response.text
+# Inisialisasi virtual display untuk menjalankan browser tanpa GUI (headless)
+display = Display(visible=0, size=(800, 800))
+display.start()
 
-def test_peminjaman():
-    url = "http://localhost:5000/peminjaman"
-    response = requests.get(url)
-    assert response.status_code == 200
-    assert "Form Peminjaman Buku" in response.text
+# Menggunakan chromedriver_autoinstaller untuk menginstal ChromeDriver
+chromedriver_autoinstaller.install()
 
-def test_peminjaman_submission():
-    url = "http://localhost:5000/peminjaman"
-    data = {
-        "nama_peminjam": "John Doe",
-        "judul_buku": "DevOps 101",
-        "tanggal_peminjaman": "2023-08-31"
-    }
-    response = requests.post(url, data=data)
-    assert response.status_code == 200
+# Konfigurasi opsi Chrome
+chrome_options = webdriver.ChromeOptions()
+
+# Daftar opsi yang ingin ditambahkan
+options = [
+    "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+]
+
+# Menambahkan opsi-opsi ke dalam chrome_options
+for option in options:
+    chrome_options.add_argument(option)
+
+class TestLibraryapptesting():
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome(options=chrome_options)
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_libraryapptesting(self):
+    self.driver.get("http://localhost:5000/")
+    self.driver.find_element(By.XPATH, "//a[contains(text(),\'Peminjaman Buku\')]").click()
+    self.driver.find_element(By.XPATH, "//input[@name=\'nama_peminjam\']").click()
+    self.driver.find_element(By.XPATH, "//input[@name=\'nama_peminjam\']").send_keys("rahil")
+    self.driver.find_element(By.XPATH, "//input[@name=\'judul_buku\']").click()
+    self.driver.find_element(By.XPATH, "//input[@name=\'judul_buku\']").send_keys("python 101")
+    self.driver.find_element(By.XPATH, "//input[@name=\'tanggal_peminjaman\']").click()
+    self.driver.find_element(By.XPATH, "//input[@name=\'tanggal_peminjaman\']").click()
+    self.driver.find_element(By.XPATH, "//input[@name=\'tanggal_peminjaman\']").send_keys("2023-09-06")
+    self.driver.find_element(By.XPATH, "//button[contains(.,\'Pinjam Buku\')]").click()
+    self.driver.find_element(By.XPATH, "//a[contains(@href, \'/\')]").click()
+    self.driver.find_element(By.XPATH, "//a[contains(text(),\'Peminjaman Buku\')]").click()
